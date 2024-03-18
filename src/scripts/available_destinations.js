4c3afecad3059ea019b59fb282a4694c3d61e3ad
Sebastian Apostu <sebiapostu@gmail.com>
2024-03-18 14:32:23 +0100

    search_destination se carga dinamicamente desde la base de datos

---
+++ src/scripts/available_destinations.js
@@ -0,0 +1,5 @@
+import { initializeFirebase } from "../firebase/firebaseConnection.js";
+import { doc, getDoc } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+
+// Inicializa Firebase
+const { app, db } = initializeFirebase();
